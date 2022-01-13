255ed41c0d6859169dbd862b2478e7cb7f8ad2c8
OCY <zichengou@gmail.com>
2022-01-13 14:56:47 +0800

    hdl

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const { thsBot } = require('@iii8iii/thsbot');
 const { WechatBot } = require('@iii8iii/wechatbot');
 require('dotenv').config();
 (async () => {
-	const browser = await firefox.launch({ headless: false });
+	const hdl = process.env.NODE_ENV === 'production';
+	const browser = await firefox.launch({ headless: hdl });
 	const ctx = await browser.newContext();
 	const wechatBot = new WechatBot(process.env.botUrls.split(','));
 	const ths = new thsBot(ctx, process.env.USER, process.env.USERPSW, wechatBot);
