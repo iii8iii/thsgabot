9453531ac98cec2933292d9a2cbfcfc6fe1c288d
OCY <zichengou@gmail.com>
2022-01-17 13:52:38 +0800

    update

--- index.js
+++ index.js
@@ -10,35 +10,30 @@ const {
 const _ = require('lodash');
 require('dotenv').config();
 
-(async () => {
+const start = async () => {
 	const hdl = process.env.NODE_ENV === 'production';
 	const browser = await firefox.launch({ headless: hdl });
 	const ctx = await browser.newContext();
 	const wechatBot = new WechatBot(process.env.botUrls.split(','));
 	const ths = new thsBot(ctx, process.env.USER, process.env.USERPSW, wechatBot);
-        const start = Date.now()
+	const codes = await getCodes();
+	await ths.update(codes, '399006');
+};
 
-	const update = async (hs=1) => {
-		console.log('---------------begin---------------');
-		let codes = [];
+const getCodes = async () => {
+	let codes = [];
 
-		const zt = await getZtStocksInfo();
-		const qs = await getQsStocksInfo(500);
+	const zt = await getZtStocksInfo();
+	const qs = await getQsStocksInfo(500);
 
-		const qsDiffZt = _.differenceBy(qs, zt, 'c');
-		for (const q of qsDiffZt) {
-			const { c, zdp } = q;
-                        let dData=undefined
-                        zdp>-5&& (dData = await getKlineData(c, 'D'));
-			dData&&ljxt(dData) && codes.push(c);
-		}
+	const qsDiffZt = _.differenceBy(qs, zt, 'c');
+	for (const q of qsDiffZt) {
+		const { c, zdp } = q;
+		let dData = undefined;
+		zdp > -5 && (dData = await getKlineData(c, 'D'));
+		dData && ljxt(dData) && codes.push(c);
+	}
+	return codes;
+};
 
-		console.log('---------------update---------------');
-		await ths.update(codes, '399006');
-
-		console.log('update done, waitting for next run...');
-		Date.now()-start<hs*60*60*1000&&update();
-	};
-
-	update();
-})();
+start();
